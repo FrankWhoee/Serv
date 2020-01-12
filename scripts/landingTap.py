6fb6aaa19693e6db88275d872445c4ba648d69d2
krowngold <Noah.Krohngold@gmail.com>
2020-01-11 21:36:53 -0800

    Fixed an issue where customer ids weren't being properly generated. Changed the customers collection to hold documents where the ids are the customer's place in line/place they entered relative to everyone else.

--- scripts/landingTap.py
+++ scripts/landingTap.py
@@ -3,7 +3,6 @@ import os
 import json
 import datetime
 import time
-import phonenumbers
 from google.cloud import firestore
 from flask import Flask, request, render_template, send_from_directory, session, flash, redirect
 from flask_wtf import FlaskForm
@@ -14,34 +13,30 @@ from app import *
 
 class EnterQueueForm(FlaskForm):
     name = StringField("name", validators=[DataRequired()], render_kw={"placeholder": "required"})
-    party_size = IntegerField('party size', validators=[DataRequired(),InputRequired()], render_kw={"value": 1, "placeholder": "required"})
-    phone_number = StringField('phone number', validators=[DataRequired(),InputRequired()], render_kw={"placeholder": "required"})
-    submit = SubmitField('continue →')
-
-    def validate_phone(self, phone):
-        try:
-            p = phonenumbers.parse(phone_number.data)
-            if not phonenumbers.is_valid_number(p):
-                raise ValueError()
-        except (phonenumbers.phonenumberutil.NumberParseException, ValueError):
-            raise ValidationError('Invalid phone number')
-
+    party_size = IntegerField('party size', validators=[DataRequired(),InputRequired()], render_kw={"value": 1})
+    phone_number = IntegerField('phone number', validators=[DataRequired(),InputRequired()], render_kw={"placeholder": "required"})
+    submit = SubmitField('Enter Queue')
 
 @app.route("/tap", methods=['GET', 'POST'])
 def landingTap_req_get():
     serviceID = request.args['service_id']
-    merchantRef = db.collection("services").document(serviceID)
+    merchantRef = db.collection("services").document(serviceID).collection("customers")
     form = EnterQueueForm()
     if form.validate_on_submit():
-        print(merchantRef)
-        data = {
+        customers = merchantRef.stream()
+        numCustomers = 0
+        for customer in customers:
+            numCustomers = numCustomers + 1
+        newData = {
             u'name'  : form.name.data,
-            u'enqueue_time' : firestore.SERVER_TIMESTAMP,
-            u'phone_number' : form.phone_number.data,
-            u"party_size" : form.party_size.data
+            u'enqueue_time' : int(time.time()),
+            u'phone_number' : str(form.phone_number.data),
+            u'party_size' : form.party_size.data,
+            u'customer_id' : numCustomers,
         }
-        merchantRef.update(data)
+        merchantRef.document(str(numCustomers)).set(newData)
+        print(numCustomers)
         flash('Login requested for user {}, phone number {}'.format(
             form.name.data, form.phone_number.data))
-        return render_template("confirmation.html")
+        return redirect("/status?service_id="+serviceID+"&customer_id="+str(numCustomers))
     return render_template("landingTap.html", form=form, serviceID=serviceID)
