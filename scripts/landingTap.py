c5d0336ab73014777377885afd45fdf9d872a704
m <mbe_pro@outlook.com>
2020-01-11 18:39:57 -0800

    Fixes to landing page & css something

--- scripts/landingTap.py
+++ scripts/landingTap.py
@@ -11,14 +11,10 @@ from wtforms.validators import DataRequired, InputRequired
 from os import environ
 from app import *
 
-class PhoneNumberForm(FlaskForm):
-    country_code = IntegerField('Country Code', validators=[DataRequired(),InputRequired()])
-    area_code = IntegerField('Area Code', validators=[DataRequired(), InputRequired()])
-    number = IntegerField('Number', validators=[DataRequired(), InputRequired()])
-
 class EnterQueueForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
-    phone_number = FormField(PhoneNumberForm)
+    party_size = IntegerField('Party Size', validators=[DataRequired(),InputRequired()])
+    phone_number = IntegerField('Phone Number', validators=[DataRequired(),InputRequired()])
     submit = SubmitField('Enter Queue')
 
 @app.route("/tap", methods=['GET', 'POST'])
@@ -30,8 +26,8 @@ def landingTap_req_get():
         data = {
             u'name'  : form.name.data,
             u'enqueue_time' : firestore.SERVER_TIMESTAMP,
-            u'phone_number' : '' + str(form.phone_number.country_code.data) + '-' + str(form.phone_number.area_code.data) + 
-            '-' + str(form.phone_number.number.data),
+            u'phone_number' : str(form.phone_number.data),
+            u"party_size" : form.party_size.data
         }
         merchantRef.update(data)
         flash('Login requested for user {}, phone number {}'.format(
