122718d0164697923abfe6a22e9b8a905a09902c
Frank Hui <huifrank@live.com>
2020-01-11 16:07:51 -0800

    Add firebase stuff

--- scripts/lineStatus.py
+++ scripts/lineStatus.py
@@ -9,7 +9,29 @@ from wtforms import SelectMultipleField, TextAreaField, SubmitField, StringField
 from wtforms.validators import DataRequired
 from os import environ
 from app import *
+from firebase import Firebase
 
+config = {
+  "apiKey": firebaseAPIKey,
+  "authDomain": "serv-91a70.firebaseapp.com",
+  "databaseURL": "https://serv-91a70.firebaseio.com/"
+}
+
+firebase = Firebase(config)
+# Get a reference to the database service
+db = firebase.database()
+
+# data to save
+data = {
+    "name": "Joe Tilsed"
+}
+
+# Pass the user's idToken to the push method
+results = db.child("users").push(data, user['idToken'])
 @app.route("/status")
 def lineStatus_req():
-    return render_template("lineStatus.html")
+
+
+    return render_template("lineStatus.html", avgTime="4:20", waitedTime="2:00", place=5)
+
+
