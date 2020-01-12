8a4f2d9148828517e8e87685e12cacc9cef4bbcc
krowngold <Noah.Krohngold@gmail.com>
2020-01-11 18:11:47 -0800

    Firestore problems???

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import os
 import json
 import datetime
 import time
+from google.cloud import firestore
 from flask import Flask, request, render_template, send_from_directory, session, flash, redirect
 from flask_wtf import FlaskForm
 from wtforms import SelectMultipleField, TextAreaField, SubmitField, StringField
 from wtforms.validators import DataRequired
 from os import environ
+
+db = firestore.Client()
 
 app = Flask(__name__)
 if __name__ == '__main__':
