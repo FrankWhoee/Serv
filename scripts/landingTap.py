ae86021b5195d9634209b9c1cbf88e4abd35749b
m <mbe_pro@outlook.com>
2020-01-11 20:15:22 -0800

    please stop toushing this

--- scripts/landingTap.py
+++ scripts/landingTap.py
@@ -3,6 +3,7 @@ import os
 import json
 import datetime
 import time
+import phonenumbers
 from google.cloud import firestore
 from flask import Flask, request, render_template, send_from_directory, session, flash, redirect
 from flask_wtf import FlaskForm
@@ -13,24 +14,33 @@ from app import *
 
 class EnterQueueForm(FlaskForm):
     name = StringField("name", validators=[DataRequired()], render_kw={"placeholder": "required"})
-    party_size = IntegerField('party size', validators=[DataRequired(),InputRequired()], render_kw={"value": 1})
-    phone_number = IntegerField('phone number', validators=[DataRequired(),InputRequired()], render_kw={"placeholder": "required"})
-    submit = SubmitField('Enter Queue')
+    party_size = IntegerField('party size', validators=[DataRequired(),InputRequired()], render_kw={"value": 1, "placeholder": "required"})
+    phone_number = StringField('phone number', validators=[DataRequired(),InputRequired()], render_kw={"placeholder": "required"})
+    submit = SubmitField('continue')
+
+    def validate_phone(self, phone):
+        try:
+            p = phonenumbers.parse(phone_number.data)
+            if not phonenumbers.is_valid_number(p):
+                raise ValueError()
+        except (phonenumbers.phonenumberutil.NumberParseException, ValueError):
+            raise ValidationError('Invalid phone number')
+
 
 @app.route("/tap", methods=['GET', 'POST'])
 def landingTap_req_get():
     serviceID = request.args['service_id']
-    merchantRef = db.collection("services").document(serviceID).collection("customers")
+    merchantRef = db.collection("services").document(serviceID)
     form = EnterQueueForm()
     if form.validate_on_submit():
         data = {
             u'name'  : form.name.data,
-            u'enqueue_time' : int(time.time()),
-            u'phone_number' : str(form.phone_number.data),
-            u'party_size' : form.party_size.data,
+            u'enqueue_time' : firestore.SERVER_TIMESTAMP,
+            u'phone_number' : form.phone_number.data,
+            u"party_size" : form.party_size.data
         }
-        merchantRef.add(data)
+        merchantRef.update(data)
         flash('Login requested for user {}, phone number {}'.format(
             form.name.data, form.phone_number.data))
-        return redirect("/status?service_id="+serviceID+"&customer_id="+"1234")
+        return render_template("confirmation.html")
     return render_template("landingTap.html", form=form, serviceID=serviceID)
