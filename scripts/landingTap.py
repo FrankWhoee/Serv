c13d91a5a0587099785c9558e91c94c8be2161c8
krowngold <Noah.Krohngold@gmail.com>
2020-01-11 18:06:55 -0800

    Revert "Working on updating the database with users as they enter their information into the website" i This reverts commit ede5c13d3f3dc28f125b3ac857cb8851e5b8b45e.

--- scripts/landingTap.py
+++ scripts/landingTap.py
@@ -3,7 +3,6 @@ import os
 import json
 import datetime
 import time
-from google.cloud import firestore
 from flask import Flask, request, render_template, send_from_directory, session, flash, redirect
 from flask_wtf import FlaskForm
 from wtforms import SelectMultipleField, TextAreaField, SubmitField, StringField, IntegerField, FormField
@@ -23,17 +22,9 @@ class EnterQueueForm(FlaskForm):
 
 @app.route("/tap", methods=['GET', 'POST'])
 def landingTap_req_get():
-    serviceID = request.args['service_id']
-    merchantRef = db.collections("services").document(serviceID)
     form = EnterQueueForm()
     if form.validate_on_submit():
-        data = {
-            u'name'  : form.name,
-            u'enqueue_time' : firestore.SERVER_TIMESTAMP,
-            u'phone_number' : form.phone_number.country_code + '-' + form.phone_number.area_code + '-' + form.phone_number.number,
-        }
-        merchantRef.set(data)
         flash('Login requested for user {}, phone number {}'.format(
             form.name.data, form.phone_number.data))
         return redirect(url_for('confirmation'))
-    return render_template("landingTap.html", form=form, serviceID=serviceID)
+    return render_template("landingTap.html", form=form)
