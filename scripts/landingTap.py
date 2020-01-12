61a2a74c5aa1f59c6809f6606128ab3e53012fa2
krowngold <Noah.Krohngold@gmail.com>
2020-01-11 18:41:23 -0800

    Database testing changes

--- scripts/landingTap.py
+++ scripts/landingTap.py
@@ -28,13 +28,15 @@ def landingTap_req_get():
     form = EnterQueueForm()
     if form.validate_on_submit():
         data = {
-            u'name'  : form.name.data,
-            u'enqueue_time' : firestore.SERVER_TIMESTAMP,
-            u'phone_number' : '' + str(form.phone_number.country_code.data) + '-' + str(form.phone_number.area_code.data) + 
-            '-' + str(form.phone_number.number.data),
+            "49": {
+                u'name'  : form.name.data,
+                u'enqueue_time' : int(time.time()),
+                u'phone_number' : '' + str(form.phone_number.country_code.data) + '-' + str(form.phone_number.area_code.data) + 
+                '-' + str(form.phone_number.number.data),
+            }
         }
         merchantRef.update(data)
         flash('Login requested for user {}, phone number {}'.format(
             form.name.data, form.phone_number.data))
-        return redirect(url_for('confirmation'))
+        return render_template("confirmation.html")
     return render_template("landingTap.html", form=form, serviceID=serviceID)
